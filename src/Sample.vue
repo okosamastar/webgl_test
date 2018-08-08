a1c9a8657353ed635ea641eb9f78b262f12a4f3a
okosamastar <okosama.star.inc@gmail.com>
2018-08-09 05:59:14 +0900

    initial commit

---
+++ src/Sample.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <model-gltf
+      :backgroundAlpha="0"
+      src="static/models/gltf/samplerhorn.gltf"
+      @on-load="onLoad"
+      :lights="lights"
+      backgroundColor="#000"></model-gltf>
+    <div class="example-loading" v-show="loading"></div>
+  </section>
+</template>
+
+<script>
+    import { ModelGltf } from 'vue-3d-model'
+
+    export default {
+      data() {
+        return {
+          loading: true,
+          // lights: [255,255,255]
+        }
+      },
+      methods: {
+        onLoad() {
+          this.loading = false;
+        }
+      },
+      components: {
+          ModelGltf
+      }
+    }
+</script>
